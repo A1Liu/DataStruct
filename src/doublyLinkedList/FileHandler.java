8908152e333d8495e6fe9a4d234f8757e9a0720d
Albert Liu <albertymliu@gmail.com>
2018-03-08 20:36:24 -0500

    added error trapping and extra features

--- src/doublyLinkedList/FileHandler.java
+++ src/doublyLinkedList/FileHandler.java
@@ -9,30 +9,45 @@ public class FileHandler {
 		
 	}
 	
+	/**
+	 * Parses a formatted input string into a Person object
+	 * @param input String with first name, last name, and employer name
+	 * @return a Person object that fits the description of the input string
+	 */
 	public Person rowHandler(String input) {
 		String[] names = input.split(" ",3);
-		if (names.length != 3) {
-			return null;
-		}
+		Person james;
+		if (names.length == 3)
+			james = new Person(names[0], names[1], names[2]);
+		else if (names.length == 2)
+			james = new Person(names[0],names[1]);
+		else return null;
 		
-		Person james = new Person(names[0], names[1], names[2]);
 		return james;
 	}
 	
+	/**
+	 * Reads the file that is given and creates a list from it
+	 * @param input name of file to be read
+	 * @throws IOException Throws an exception if there's a problem reading the file
+	 */
 	public void readFile(String input) throws IOException {
 
 		FileReader readFile = new FileReader(input); //or input.readLine( ) if from prompt
 		BufferedReader inFile = new BufferedReader(readFile);
 		
 		String inputString = inFile.readLine();
-		
+		int line = 1;
 		while(inputString != null) {
 			if(!inputString.equals("") && rowHandler(inputString) != null) {
-				list.addElement2(rowHandler(inputString),rowHandler(inputString).getLastName().compareTo("m")<0);
+				list.addElement(rowHandler(inputString),rowHandler(inputString).getLastName().compareTo("m")<0);
+			} else if (!inputString.equals("") && rowHandler(inputString) == null) {
+				System.out.println("Skipped line " + line + ": incorrect number of arguments.");
 			}
 				
 			
 			inputString = inFile.readLine();
+			line++;
 		}
 		
 		inFile.close();
